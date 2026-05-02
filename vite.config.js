280ad038a423e7204b8db992b86ed399ff052c50
DE長澤様 <tn@deinc.jp>
2026-05-03 03:08:55 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // index.html がプロジェクトルート、JSONは public/ に置く
-  // public/ の中身はビルド時にそのままコピーされる
+  base: '/paper-crumple/',
   server: {
     port: 5173,
     open: true,
